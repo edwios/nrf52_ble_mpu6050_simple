3575d9a1d985e233367c544b1a722f31f6c52926
edwios <edwintam@iostation.com>
2017-10-09 19:31:40 +0800

    Added more logic on file name matching

--- pca10040/s132/armgcc/genMkFilepaths.sh
+++ pca10040/s132/armgcc/genMkFilepaths.sh
@@ -19,16 +19,24 @@ SRC_FILE=""
 INC_PATH=""
 FILES=`awk '/^#define.*ENABLED/ {print tolower($2)}' $APP_CONF | sed -e 's/_enabled*/.c/g'`
 for i in $FILES; do
+	j=$i
+	HAS_=`echo $i | egrep '_' - ; echo $?`
+	if [ "$HAS_" == "1" ]; then
+		i="nrf_drv_$i"
+	fi
 	CPATH=`find $SDK_ROOT -name $i -print`
 	if [ "$CPATH" == "" ]; then
-		CPATH=`find $SDK_ROOT -name nrf_drv_$i -print`
+		CPATH=`find $SDK_ROOT -name $j -print`
+	fi
+	if [ "$CPATH" == "" ]; then
+		CPATH=`find $SDK_ROOT -name "app_$j" -print`
 	fi
 	if [ "$CPATH" != "" ]; then
 		SRC_FILE="${SRC_FILE} `echo $CPATH | sed -e \"s#^$SDK_ROOT#\$\(SDK_ROOT\)#\"`"
 		INC_PATH="${INC_PATH} `dirname $CPATH | sed -e \"s#^$SDK_ROOT#\$\(SDK_ROOT\)#\"`"
 		# echo "Found $i"
 	else
-		echo "WARNING: $i not found"
+		echo "WARNING: $j not found"
 	fi
 done
 echo "SRC_FILES += \\"
